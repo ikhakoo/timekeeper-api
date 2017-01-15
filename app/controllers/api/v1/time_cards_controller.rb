012a17bda6a532d32c5dccdd4e00c3799c2e395e
ikhakoo <ikhakoo@gmail.com>
2017-01-15 15:51:43 -0500

    add total hours

--- app/controllers/api/v1/time_cards_controller.rb
+++ app/controllers/api/v1/time_cards_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::TimeCardsController < ApplicationController
   before_action :set_time_card, only: [:show, :update, :destroy]
 
-  # GET /time_cards
+  # GET /api/v1/time_cards
   def index
     @time_cards = TimeCard.all
 
     render json: @time_cards, include: :time_entries
   end
 
-  # GET /time_cards/1
+  # GET /api/v1/time_cards/1
   def show
     render json: @time_card, include: :time_entries
   end
 
-  # POST /time_cards
+  # POST /api/v1/time_cards
   def create
     @time_card = TimeCard.new(time_card_params)
 
@@ -24,7 +24,7 @@ class Api::V1::TimeCardsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /time_cards/1
+  # PATCH/PUT /api/v1/time_cards/1
   def update
     if @time_card.update(time_card_params)
       render json: @time_card
@@ -33,18 +33,21 @@ class Api::V1::TimeCardsController < ApplicationController
     end
   end
 
-  # DELETE /time_cards/1
+  # DELETE /api/v1/time_cards/1
   def destroy
-    @time_card.destroy
+    if @time_card.present?
+    	@time_card.destroy 
+    	head :ok
+    else
+    	render json: @time_card.errors, status: :unprocessable_entity
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_time_card
       @time_card = TimeCard.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def time_card_params
       params.require(:time_card).permit(:username, :occurrence)
     end
--- app/controllers/api/v1/time_entries_controller.rb
+++ app/controllers/api/v1/time_entries_controller.rb
@@ -1,30 +1,31 @@
 class Api::V1::TimeEntriesController < ApplicationController
   before_action :set_time_entry, only: [:show, :update, :destroy]
 
-  # GET /time_entries
+  # GET /api/v1/time_entries
   def index
     @time_entries = TimeEntry.all
 
     render json: @time_entries
   end
 
-  # GET /time_entries/1
+  # GET /api/v1/time_entries/1
   def show
     render json: @time_entry
   end
 
-  # POST /time_entries
+  # POST /api/v1/time_entries
   def create
     @time_entry = TimeEntry.new(time_entry_params)
 
     if @time_entry.save
+      check_time_entry_card_count(time_entry)
       render json: @time_entry, status: :created, location: @time_entry
     else
       render json: @time_entry.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /time_entries/1
+  # PATCH/PUT /api/v1/time_entries/1
   def update
     if @time_entry.update(time_entry_params)
       render json: @time_entry
@@ -33,19 +34,33 @@ class Api::V1::TimeEntriesController < ApplicationController
     end
   end
 
-  # DELETE /time_entries/1
+  # DELETE /api/v1/time_entries/1
   def destroy
-    @time_entry.destroy
+    if @time_entry.present?
+      @time_entry.destroy, head :ok
+    else
+      render json: @time_entry.errors, status: :unprocessable_entity
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_time_entry
       @time_entry = TimeEntry.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def time_entry_params
       params.require(:time_entry).permit(:time, :time_card_id)
     end
+
+    def check_time_entry_card_count(time_entry)
+      time_card = time_entry.time_card
+      if time_card.present? && time_card.entries.count == 2
+        calculate_total_hours(time_card)
+      end
+    end
+
+    def calculate_total_hours(time_card)
+      time_card.update!(total_hours: ((time_card.entries.second.time - time_card.entries.first.time) / 1.hour).round)
+    end
+
 end
